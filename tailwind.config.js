b69927dcc95631f7c7101aab57286cbd2dd63c26
milan090 <milanmuhammed1@gmail.com>
2023-05-27 23:11:53 +0530

    🚀 hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,12 @@
-const { fontFamily } = require('tailwindcss/defaultTheme');
+const { fontFamily } = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
       backgroundImage: {
+        "border-gradient-to-r": "linear-gradient(to right, white, transparent)",
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
@@ -18,10 +17,24 @@ module.exports = {
         redhat: ["var(--redhat-font)", ...fontFamily.serif],
       },
       colors: {
-        background: "#060606"
-      }
+        background: "#060606",
+      },
     },
   },
 
-  plugins: [],
+  plugins: [
+    function ({ addUtilities }) {
+      const newUtilities = {
+        ".border-gradient-right": {
+          border: "double 1px transparent",
+          backgroundImage: "var(--tw-gradient)",
+          backgroundOrigin: "border-box",
+          backgroundClip: "content-box, border-box",
+          boxDecorationBreak: "clone",
+        },
+      };
+
+      addUtilities(newUtilities, ["responsive", "hover"]);
+    },
+  ],
 };
