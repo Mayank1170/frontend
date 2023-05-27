2eb73a3b92a6deec16bdb8c6886f82418407465d
milan090 <milanmuhammed1@gmail.com>
2023-05-27 13:49:14 +0530

    🚀  init repo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,24 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import { pilatExtended } from "@/utils/fonts";
+import classNames from "classnames";
+import type { AppProps } from "next/app";
+import { Red_Hat_Display } from "next/font/google";
+
+const redhat = Red_Hat_Display({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+     <style jsx global>
+        {`
+          :root {
+            --redhat-font: ${redhat.style.fontFamily};
+            --pilat-font: ${pilatExtended.style.fontFamily};
+          }
+        `}
+      </style>
+      <Component {...pageProps} />
+    </>
+    
+  );
 }
---
+++ src/utils/fonts.ts
@@ -0,0 +1,36 @@
+import localFont from "next/font/local";
+
+export const pilatExtended = localFont({
+  src: [
+    {
+      path: "./PilatExtended/PilatExtended-Thin.woff2",
+      weight: "200",
+      style: "normal",
+    },
+    {
+      path: "./PilatExtended/PilatExtended-Light.woff2",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "./PilatExtended/PilatExtended-Regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "./PilatExtended/PilatExtended-Medium.woff2",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "./PilatExtended/PilatExtended-SemiBold.woff2",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "./PilatExtended/PilatExtended-Bold.woff2",
+      weight: "700",
+      style: "normal",
+    },
+  ],
+});
