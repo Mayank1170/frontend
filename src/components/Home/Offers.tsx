f8d64b8c00986427f8d58eeee8dceb1aa4230e46
milan090 <milanmuhammed1@gmail.com>
2023-06-02 08:39:06 +0530

    fix infi scroll

--- src/components/Home/Offers.tsx
+++ src/components/Home/Offers.tsx
@@ -5,73 +5,55 @@ const offers: OfferProps[] = [
   {
     icon: "/images/offers/zero-slippage.svg",
     title: "Zero Slippage",
+    id: 1,
   },
   {
     icon: "/images/offers/lp-profit.svg",
     title: "Tailored for LP profitability",
+    id: 2,
   },
   {
     icon: "/images/offers/min-price.svg",
     title: "Minimum price impact",
+    id: 3,
   },
 
   {
     icon: "/images/offers/assured-safety.svg",
     title: "Assured safety of LP funds",
+    id: 4,
   },
   {
     icon: "/images/offers/optimal-cap.svg",
     title: "Optimal capital efficiency",
+    id: 5,
   },
-  {
-    icon: "/images/offers/badhiya-security.svg",
-    title: "Badhiya security",
-  }
-
 ];
+
+// offers.push(...offers);
 
 const Offers: React.FC = () => {
   const [cards, setCards] = useState(offers);
-  const [scrollPosition, setScrollPosition] = useState(0);
-  const containerRef = useRef<HTMLDivElement>(null);
-
-  const scrollSpeed = 50;
-
-  useEffect(() => {
-    const tickerInterval = setInterval(() => {
-      setScrollPosition((prevPosition) => prevPosition + 1);
-    }, 1000 / scrollSpeed);
-
-    if (containerRef.current && containerRef.current.parentElement) {
-      containerRef.current.parentElement.style.paddingBottom =
-        containerRef.current.offsetHeight + "px";
-    }
-
-    return () => clearInterval(tickerInterval);
-  }, []);
-
-  useEffect(() => {
-    const containerWidth = containerRef.current?.offsetWidth || 0;
-    if (scrollPosition >= containerWidth / 3) {
-      setScrollPosition(scrollPosition - containerWidth / 2);
-      setCards((prevCards) => [...prevCards.slice(1), prevCards[0]]);
-    }
-  }, [scrollPosition]);
-
 
   return (
-    <div className="relative pt-8 overflow-x-scroll w-screen scroll scroll-hide">
-      <div
-        ref={containerRef}
-        className="absolute gap-x-[30px] top-0 flex flex-nowrap  pt-10"
-        style={{
-          transform: `translateX(-${scrollPosition}px)`,
-        }}
-      >
-        {cards.map((offer, index) => (
-          <Offer key={index} {...offer} />
+    <div className="flex gap-x-8 mt-10">
+      <section>
+        {cards.map((card) => (
+          <Offer {...card} key={card.id} />
         ))}
-      </div>
+      </section>
+
+      <section>
+        {cards.map((card) => (
+          <Offer {...card} key={card.id} />
+        ))}
+      </section>
+
+      <section>
+        {cards.map((card) => (
+          <Offer {...card} key={card.id} />
+        ))}
+      </section>
     </div>
   );
 };
@@ -79,6 +61,7 @@ const Offers: React.FC = () => {
 interface OfferProps {
   icon: string;
   title: string;
+  id: number;
 }
 
 const Offer: React.FC<OfferProps> = ({ icon, title }) => {
