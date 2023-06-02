a5ea312ac2d9f55a9acf6f1f0b57993aea2c6b92
milan090 <milanmuhammed1@gmail.com>
2023-06-02 08:56:15 +0530

    add divider

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,11 +14,18 @@ const Home: NextPageWithLayout = () => {
     >
       <Hero />
       <div
-        className="bg-[url('/images/content-bg.svg')]"
+        className="bg-[url('/images/content-bg.svg')] relative"
         style={{
           backgroundSize: "cover",
         }}
       >
+        <div
+          className="w-[15px] h-[2100px] absolute top-[160px] right-[50%] bg-[#060606] z-[100] opacity-30"
+          style={{
+            background:
+              "linear-gradient(180deg, rgba(59, 176, 120, 0.15) 3.79%, #3BB078 27.85%, rgba(59, 176, 120, 0.936441) 51.9%, rgba(59, 176, 120, 0.843548) 75.95%, rgba(59, 176, 120, 0.15) 100%)",
+          }}
+        />
         <Content />
 
         <hr className="max-w-[1200px] mx-auto my-[60px] opacity-30" />
