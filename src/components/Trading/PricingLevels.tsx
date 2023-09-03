a6854de9a68f4606bc9f63fde14760fbbee81eac
Mayank1170 <agarwalmayank1170@gmail.com>
2023-09-03 14:47:50 +0530

    Made order type drop down

--- src/components/Trading/PricingLevels.tsx
+++ src/components/Trading/PricingLevels.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { BiChevronDown } from 'react-icons/bi'
 import { BiChevronUp } from 'react-icons/bi'
 import { RiSearch2Line } from 'react-icons/ri'
