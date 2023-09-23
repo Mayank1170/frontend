accd1e55731167b2397d2c0743a9880142775b58
Mayank Agarwal <agarwalmayank1170@gmail.com>
2023-09-23 11:59:30 +0000

    Fixed sol-perp dropdown bug

--- src/components/Trading/PricingLevels.tsx
+++ src/components/Trading/PricingLevels.tsx
@@ -38,7 +38,7 @@ const GeneralInfo: React.FC = () => {
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
       if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
-        setIsOpen(false);
+        setIsOpen(true);
       }
     };
 
