0de68710c512b332e92240eae3d4c821f8f02cc8
Anish De <63192115+AnishDe12020@users.noreply.github.com>
2023-10-14 17:49:38 +0530

    feat: account info and open order hook

---
+++ src/hooks/useAccountInfo.tsx
@@ -0,0 +1,28 @@
+import { useQuery } from "@tanstack/react-query";
+import useTRGs from "./useTRGs";
+import { useWallet } from "@solana/wallet-adapter-react";
+import { getAccountInfo } from "@/utils/dexterity";
+import useDexterity from "./useDexterity";
+
+const useAccountInfo = () => {
+  const { trgs } = useTRGs();
+  const { manifest } = useDexterity();
+  const { publicKey } = useWallet();
+
+  const { data: accountInfo } = useQuery({
+    queryKey: ["accountInfo", publicKey?.toBase58()],
+    queryFn: async () => {
+      return await getAccountInfo(manifest!, trgs![0].pubkey);
+    },
+    enabled:
+      !!manifest &&
+      !!publicKey &&
+      !!manifest.fields.wallet?.publicKey &&
+      !!trgs &&
+      trgs.length > 0,
+  });
+
+  return { accountInfo };
+};
+
+export default useAccountInfo;
